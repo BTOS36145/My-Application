04ffa7aed4b2ea0de6f033a286720284d6df4789
BTOS36145 <briiiiiiiiiiiian@gmail.com>
2020-11-13 22:57:45 +0000

    added SQLite db

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,71 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //refs to buttons
+    Button btn_add, btn_viewAll;
+    EditText et_name, et_age;
+    Switch sw_activeCustomer;
+    ListView lv_customerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        sw_activeCustomer = findViewById(R.id.sw_active);
+        et_name = findViewById(R.id.et_name);
+        et_age =findViewById(R.id.et_age);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+        //click listeners
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                CustomerModel customerModel;
+                try {
+                    customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error Creating Customer", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1, "error", 0, false);
+
+
+                }
+
+                DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
+
+                boolean success = dataBaseHelper.addOne(customerModel);
+
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "View All Button", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+
     }
 }
